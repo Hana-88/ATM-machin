80b353374d5d27b47b4936800404e42490b9f314
Hana-88 <79525367+Hana-88@users.noreply.github.com>
2021-11-09 11:16:56 +0300

    Update account.java

--- ATM/account.java
+++ ATM/account.java
@@ -2,118 +2,164 @@
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.io.IOException;
+import java.io.Serializable;
 
-public class account {
+public class Account extends deposit_withdraw implements Serializable {
 
-	Scanner s=new Scanner(System.in);
-	HashMap<Integer,Integer> m=new HashMap<Integer,Integer>();
-	 DecimalFormat f=new DecimalFormat("'$'###,##0.00");
-	public int getcustemer()
-	{
-		return custemer;
+	Scanner scanner = new Scanner(System.in);
+	HashMap<Integer, Integer> p = new HashMap<Integer, Integer>();
+	DecimalFormat d = new DecimalFormat("'$'###,##0.00");
+
+	public void log() throws IOException {
+		int x = 1;
+
+		do {
+
+			try {
+
+				System.out.println("welcom \n");
+                if(p.isEmpty())
+				{
+					 
+					System.out.println("Create your Atm Account  ");
+					System.out.println(" id");
+					int id=scanner.nextInt();
+					System.out.println("passowrd");
+					int passowrd=scanner.nextInt();
+					p.put(id, passowrd);
+					
+
+					getAcounttype();
+				}
+				 
+			 
+                 else{
+					System.out.println("enter your id \n");
+					SetCustemernumber(s.nextInt());
+					System.out.println("Enter your passowrd \n ");
+					setIp(s.nextInt());
+				 }
+				 
+			 
+
+			} catch (Exception e) {
+				System.out.println("invalid inpute\n");
+				x = 2;
+			}
+           
+			if(p!=null)
+			{
+				for (Entry<Integer, Integer> entry : p.entrySet()) {
+					if (entry.getKey() == getcustemer() && entry.getValue() == getip()) {
+						getAcounttype();
+					}
+	
+				}
+			}
+			 
+
+			System.out.println("invalid inpute\n");
+		}
+
+		while (x == 1);
+
 	}
-	 public void SetCustemernumber(int custemer)
-	 {
-		this.custemer=custemer; 
-	 }
 
-		public int getip()
-		{
-			return ip;
+	public void getAcounttype() throws IOException {
+		System.out.println("please choose one option below \n");
+		System.out.println("1- checking");
+		System.out.println("2- deposite");
+		System.out.println("3- withdraw");
+		System.out.println("4- delete your account");
+		System.out.println("5- exite");
+
+		select = s.nextInt();
+
+		switch (select) {
+		case 1:
+			getChecking();
+			break;
+
+		case 2:
+			getdepose();
+			break;
+		case 3:
+			getwithdraw();
+          break;
+		  case 4: delete();
+		       System.out.println("your account is deleted");  log(); 
+			   break;
+		case 5:
+			System.out.println("exited");
+			break;
+		default:
+			System.out.println("invalide inpute");
+			getAcounttype();
 		}
-	 public void setIp(int ip)
-	 {
-		 this.ip=ip;
-	 }
-	 
-	 
-	 public double calckchekdepos(double amount)
-	 {
-		 chek=chek+amount;
-        return chek;
-	 }
-	 public double calcsavdepos(double amount)
-	 {
-		 sav=sav+amount;
-        return sav;
-	 }
-	 public void getchekingdeposinput()
-	 {
-		System.out.println("chekar :"+f.format(chek)) ;
-		System.out.println("brakai chan we");
-		
-		double amount=s.nextDouble();
-		
-		calckchekdepos(amount);
-		System.out.println("cheka tazakat brytya la"+f.format(chek));
-	 
-	 }
-	 public void getsavingdeposinput()
-	 {
-		System.out.println("chekar :"+f.format(sav)) ;
-		System.out.println("brakai chan we");
-		
-		double amount=s.nextDouble();
-		
-		calckchekdepos(amount);
-		System.out.println("cheka tazakat brytya la"+f.format(sav));
-	 
-	 }
-	 
-	 public double calckchekwithdraw(double amount)
-	 {
-		 chek=chek-amount;
-		 return chek;
-	 }
-	 public double calcksavwithdraw(double amount)
-	 {
-		 sav=sav-amount;
-		 return sav;
-	 }
-	 public void getCheckingwithdrawinput()
-	 {
-		System.out.println("chekar :"+f.format(chek)) ;
-		System.out.println("brakai chan we");
-		
-		double amount=s.nextDouble();
-		if(chek-amount>=0)
-		{
-		calckchekwithdraw(amount);
-		System.out.println("cheka tazakat brytya la"+f.format(chek));
+
+	}
+
+	public void getChecking() throws IOException {
+
+		System.out.println("your balance is " + d.format(get_checking_balance()));
+		getAcounttype();
+
+	}
+
+	public void getdepose() throws IOException {
+		System.out.println("please choose one option below in checking");
+		System.out.println("1-show my acount");
+		System.out.println("2- deposite");
+		System.out.println("3- Exite");
+
+		select = scanner.nextInt();
+		switch (select) {
+		case 1:
+			System.out.println("your amount of money is :" + d.format(get_checking_balance()));
+			getAcounttype();
+			break;
+		case 2:
+			getchekingdeposinput();
+			getAcounttype();
+			break;
+
+		case 3:
+			System.out.println("exited");
+
+			break;
 		}
-		else
-		{
-			System.out.println("negativa nabet");
+	}
+
+	public void getwithdraw() throws IOException {
+		System.out.println("please choose one option below in checking");
+		System.out.println("1-show my acount");
+		System.out.println("2- withdraw fund");
+		System.out.println("3- Exite");
+
+		select = scanner.nextInt();
+		switch (select) {
+		case 1:
+			System.out.println("your amount of money is :" + d.format(get_checking_balance()));
+			getAcounttype();
+			break;
+		case 2:
+			checking_withdraw();
+			getAcounttype();
+			break;
+		case 3:
+			System.out.println("exited");
+
+			break;
 		}
-	 }
-	 public void getsavinggwithdrawinput()
-	 {
-		System.out.println("chekar :"+f.format(sav)) ;
-		System.out.println("brakai chan be");
-		
-		double amount=s.nextDouble();
-		if(sav-amount>=0)
-		{
-		calcksavwithdraw(amount);
-		System.out.println("cheka tazakat brytya la"+f.format(sav));
-		}
-		else
-		{
-			System.out.println("negativa nabet");
-		}
-	 }
-	 public double getchekingbalance()
-	 {
-		 return chek;
-	 }
-	 public double getsavingbalance()
-	 {
-		 return sav;
-	 }
-	 
-	 private int custemer;
-private int ip;
-private double chek=0;
-private double sav=0;
+	}
+	public void delete()
+	{
+		p.clear();
+	}
+
+	private int select;
+
 }
